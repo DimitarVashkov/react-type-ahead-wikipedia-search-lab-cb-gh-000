e88800c1a749643ba6982d64460c77192919d2d2
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-19 23:23:18 +0000

    automatically backed up by learn

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -5,7 +5,7 @@ import React from 'react';
 const SearchResults = ({ results }) => (
   <ul className='search-results'>
   {results.map((result,index)=>
-    <li><p> result.description </p> </li>
+    <li><a href={result.link}>{result.title}</a><p> {result.description} </p> </li>
   )}
   </ul>
 );
